d607732c19c3d0c5eedf4afe7266fd9c9393e007
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-09-06 16:42:04 -0500

    added buttons and labels, connected storyboard to view controller

--- Flash Cards/ViewController.swift
+++ Flash Cards/ViewController.swift
@@ -14,12 +14,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var questionsLabel: UILabel!
+    
+    
+    @IBOutlet weak var aswerLabel: UILabel!
+    
+    
+    @IBAction func revealAnswer(sender: AnyObject) {
+    
     }
 
+    @IBAction func nextQuestion(sender: AnyObject) {
+        
+    }
 
 }
+
 
